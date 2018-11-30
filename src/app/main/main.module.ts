08985172beb1210c0862123364ec1d8b8b471728
LuAnderson <lucasand96@gmail.com>
2018-11-29 23:41:15 -0200

    Componente de footer do aplicativo e Ajustes de esboço

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { MenuComponent } from './menu/menu.component';
 import { CategoriasComponent } from './categorias/categorias.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     MainComponent,
     MenuComponent,
     CategoriasComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule
