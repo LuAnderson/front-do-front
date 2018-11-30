35b9742834ca0bf39147a0f1838a66d60e5b3b4c
LuAnderson <lucasand96@gmail.com>
2018-11-30 00:01:00 -0200

    Footer Componente - Esboço da Estrutura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
-import { MainModule } from './main/main.module';
-import { MenuComponent } from './main/menu/menu.component';
-import { MainComponent } from './main/main.component';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { MainModule } from './main/main.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriasComponent } from './main/categorias/categorias.component';
+import { FooterComponent } from './main/footer/footer.component';
+import { MenuComponent } from './main/menu/menu.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     MenuComponent,
-    CategoriasComponent
+    CategoriasComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
