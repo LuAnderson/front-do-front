298e1bc0c8ef8b5c4769cb27a56e5460ecdf282d
LuAnderson <lucasand96@gmail.com>
2018-11-29 23:18:41 -0200

    Componente de Categorias da Tela principal

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { MenuComponent } from './menu/menu.component';
+import { CategoriasComponent } from './categorias/categorias.component';
 
 @NgModule({
   declarations: [
     MainComponent,
     MenuComponent,
+    CategoriasComponent,
   ],
   imports: [
     CommonModule
